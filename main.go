adc57a9b96b9be151fe38363b1b67b61d6cb0007
Michael Krug <michi.krug@gmail.com>
2025-02-13 18:08:19 +0100

    Rollback pvp threshold change

--- main.go
+++ main.go
@@ -1435,8 +1435,8 @@ func filterAndSendEncounters(users FilteredUsers, encounters []EncounterData) {
 			} else {
 				for league, entries := range pvpData {
 					for _, entry := range entries {
-						if entry.Percentage > 99 {
-							log.Printf("🎉 Top %s league encounter - Pokemon: %s, CP: %d, Rank: %d, Percentage: %f, Level: %f", league, getPokemonName(entry.Pokemon, "en"), entry.CP, entry.Rank, entry.Percentage, entry.Level)
+						if entry.Rank <= 10 {
+							log.Printf("🎉 Top 10 %s league encounter - Pokemon: %s, CP: %d, Rank: %d, Percentage: %f, Level: %f", league, getPokemonName(entry.Pokemon, "en"), entry.CP, entry.Rank, entry.Percentage, entry.Level)
 						}
 					}
 				}
